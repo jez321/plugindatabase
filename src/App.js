5d249eb6e1631998fd29a6ff1a41b4de7d7a5011
Jeremy Johns <jezjez@gmail.com>
2019-07-02 00:07:19 +0900

    Revert "refactor, useEffect performance fix in DynamicTable"
    This reverts commit a36d506ef3f7db388085a71cd83f5a71205e04a2.

--- src/App.js
+++ src/App.js
@@ -4,60 +4,51 @@ import './App.css';
 import style from 'styled-components';
 import DynamicTable from './DynamicTable/DynamicTable';
 import CardList from './CardList/CardList';
-import SearchBox from './SearchBox/SearchBox';
-import * as StyleConstants from './constants/style';
-
-const mockDeals = [
-  {
-    id: 1,
-    name: 'Neutron 3',
-    company: 'Izotope',
-    details: '30% off',
-    added: '2019/6/4',
-    ends: '2019/6/30',
-    link: {
-      text: 'AudioDeluxe',
-      url: 'https://www.audiodeluxe.com/products/audio-plug-ins/izotope-neutron-3-advanced',
-    },
-  },
-  {
-    id: 2,
-    name: 'Tverb',
-    company: 'Eventide',
-    details: '70% off',
-    added: '2019/6/3',
-    ends: '2019/6/25',
-    link: { text: 'JRR Shop', url: 'https://www.jrrshop.com/eventide-tverb' },
-  },
-  {
-    id: 3,
-    name: 'Addictive Drums',
-    company: 'XLN Audio',
-    details: '50% off',
-    added: '2019/6/2',
-    ends: '2019/6/25',
-    link: { text: 'AudioDeluxe', url: 'https://www.audiodeluxe.com/products/xln-audio-addictive-drums-2-custom' },
-  },
-];
-
-const PageTitle = style.div`
-color: #aaa;
-`;
-
-const columns = [
-  { title: 'Plugin', key: 'name' },
-  { title: 'Company', key: 'company' },
-  { title: 'Details', key: 'details' },
-  { title: 'Added', key: 'added' },
-  { title: 'End', key: 'ends' },
-  { title: 'Store', key: 'link', type: 'link' },
-];
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-  const isTabletOrMobile = useMedia({ maxWidth: StyleConstants.MOBILE_MAX_WIDTH });
+  const [deals, setDeals] = useState([
+    {
+      id: 1,
+      name: 'Neutron 3',
+      company: 'Izotope',
+      details: '30% off',
+      added: '2019/6/4',
+      ends: '2019/6/30',
+      link: {
+        text: 'AudioDeluxe',
+        url: 'https://www.audiodeluxe.com/products/audio-plug-ins/izotope-neutron-3-advanced',
+      },
+    },
+    {
+      id: 2,
+      name: 'Tverb',
+      company: 'Eventide',
+      details: '70% off',
+      added: '2019/6/3',
+      ends: '2019/6/25',
+      link: { text: 'JRR Shop', url: 'https://www.jrrshop.com/eventide-tverb' },
+    },
+    {
+      id: 3,
+      name: 'Addictive Drums',
+      company: 'XLN Audio',
+      details: '50% off',
+      added: '2019/6/2',
+      ends: '2019/6/25',
+      link: { text: 'AudioDeluxe', url: 'https://www.audiodeluxe.com/products/xln-audio-addictive-drums-2-custom' },
+    },
+  ]);
+  const isTabletOrMobile = useMedia({ maxWidth: 1000 });
 
   // DynamicTable settings
+  const columns = [
+    { title: 'Plugin', key: 'name' },
+    { title: 'Company', key: 'company' },
+    { title: 'Details', key: 'details' },
+    { title: 'Added', key: 'added' },
+    { title: 'End', key: 'ends' },
+    { title: 'Store', key: 'link', type: 'link' },
+  ];
 
   // Delete a row on click -> sample
   const clickHandler = (id) => {
@@ -65,6 +56,18 @@ const App = () => {
     newRows.splice(newRows.findIndex(r => r.id === id), 1);
     setDeals(newRows);
   };
+
+  const PageTitle = style.div`
+    color: #aaa;
+  `;
+
+  const SearchBox = style.input`
+    margin-bottom: 20px;
+    border-radius: 5px;
+    border: solid 1px #ccc;
+    font-size: 22px;
+    padding: 10px 15px;
+    `;
 
   // Display cards or table depending on current width
   const dataDisplay = isTabletOrMobile
