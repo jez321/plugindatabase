f4d2697ac6cf056a0849f8f505c057ab76b3183c
Jeremy Johns <jezjez@gmail.com>
2020-01-04 11:42:49 +0900

    add redux-persist to persist wanted/owned plugins to localstorage

--- src/index.jsx
+++ src/index.jsx
@@ -7,40 +7,43 @@ import {
 } from 'react-router-dom';
 import { Security, ImplicitCallback, SecureRoute } from '@okta/okta-react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
 import App from './components/App';
 import Login from './components/Login/Login';
 import * as serviceWorker from './serviceWorker';
 import Plugins from './components/Plugins/Plugins';
 import Deals from './components/Deals/Deals';
-import store from './redux/store';
 import Profile from './components/Profile/Profile';
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Security
-        issuer={`${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`}
-        client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
-        redirect_uri={`${window.location.origin}/implicit/callback`}
-      >
-        <App>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <Redirect to="/deals" />
-              )}
-            />
-            <Route path="/login" exact render={() => <Login baseUrl="https://dev-786379.okta.com" />} />
-            <Route path="/implicit/callback" component={ImplicitCallback} />
-            <Route path="/plugins" exact component={Plugins} />
-            <Route path="/deals" exact component={Deals} />
-            <SecureRoute path="/profile" exact component={Profile} />
-          </Switch>
-        </App>
-      </Security>
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Security
+          issuer={`${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`}
+          client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
+          redirect_uri={`${window.location.origin}/implicit/callback`}
+        >
+          <App>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={() => (
+                  <Redirect to="/deals" />
+                )}
+              />
+              <Route path="/login" exact render={() => <Login baseUrl="https://dev-786379.okta.com" />} />
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+              <Route path="/plugins" exact component={Plugins} />
+              <Route path="/deals" exact component={Deals} />
+              <SecureRoute path="/profile" exact component={Profile} />
+            </Switch>
+          </App>
+        </Security>
+      </BrowserRouter>
+    </PersistGate>
   </Provider>, document.getElementById('root'),
 );
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux";
-import rootReducer from "./rootReducer";
+import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export default store;
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
