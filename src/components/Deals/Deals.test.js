94d49d3aba3a51737d4d630dabbac6f7e0a52b65
Jeremy Johns <jezjez@gmail.com>
2020-01-05 01:19:41 +0900

    add mock mediaquery to deals test

--- src/components/Deals/Deals.test.js
+++ src/components/Deals/Deals.test.js
@@ -3,24 +3,47 @@ import { shallow } from 'enzyme';
 import '../../matchMedia.mock';
 import Deals from './Deals';
 import TestUtil from '../../test/testUtil';
+import * as SC from '../../constants/Style';
 
+const mediaQueries = {
+  tabletOrMobile: { maxWidth: SC.MOBILE_MAX_WIDTH },
+};
 const setup = (props = {}, state = null) => {
   const wrapper = shallow(<Deals {...props} />);
   if (state) wrapper.setState(state);
   return wrapper;
 };
 
+afterEach(() => {
+  TestUtil.mockMatchMedia({
+    matches: false,
+  });
+});
+
 test('renders without error', () => {
   const wrapper = setup();
   expect(wrapper).toBeTruthy();
 });
 
-test('renders either table or card list', () => {
+
+test('renders either table or card list', (done) => {
+  TestUtil.mockMatchMedia({
+    media: mediaQueries.tabletOrMobile,
+    matches: true,
+  });
   const wrapper = setup();
-  const cardList = TestUtil.findByDataTestAttrVal(wrapper, 'component-card-list');
-  const dynamicTable = TestUtil.findByDataTestAttrVal(
-    wrapper,
-    'component-dynamic-table',
-  );
-  expect(cardList.length + dynamicTable.length).toBe(1);
+  setImmediate(() => {
+    wrapper.update();
+    try {
+      const cardList = TestUtil.findByDataTestAttrVal(wrapper, 'component-card-list');
+      const dynamicTable = TestUtil.findByDataTestAttrVal(
+        wrapper,
+        'component-dynamic-table',
+      );
+      expect(cardList.length + dynamicTable.length).toBe(1);
+      done();
+    } catch (error) {
+      done.fail(error);
+    }
+  });
 });
--- src/test/testUtil.js
+++ src/test/testUtil.js
@@ -1,8 +1,24 @@
 import checkPropTypes from 'check-prop-types';
+import { mockMediaQueryList } from 'use-media';
 
 class TestUtil {
   static findByDataTestAttrVal(wrapper, val) {
     return wrapper.find(`[data-test="${val}"]`);
+  }
+
+  static getMockImplementation({ media, matches = false }) {
+    const mql = {
+      ...mockMediaQueryList,
+      media,
+      matches,
+    };
+
+    return () => mql;
+  }
+
+  static mockMatchMedia({ media, matches = false }) {
+    const mockedImplementation = TestUtil.getMockImplementation({ media, matches });
+    window.matchMedia = jest.fn().mockImplementation(mockedImplementation);
   }
 
   static checkProps(component, props) {
