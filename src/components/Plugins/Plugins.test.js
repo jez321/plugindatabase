8658a73d3d2aeca0b1f83b1cb0d8b44b319391d5
Jeremy Johns <jezjez@gmail.com>
2020-02-12 00:17:56 +0900

    add moxios, add Plugins plugin link test

--- src/components/Plugins/Plugins.test.js
+++ src/components/Plugins/Plugins.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import '../../matchMedia.mock';
+import { act } from 'react-dom/test-utils';
+import moxios from 'moxios'
+import api from '../../api/api';
 import { PluginsRaw } from './Plugins';
 import TestUtil from '../../test/testUtil';
 
@@ -18,6 +21,12 @@ const defaultProps = {
 const setup = (props = {}, state = null) => {
   const setupProps = { ...defaultProps, ...props };
   const wrapper = shallow(<PluginsRaw {...setupProps} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+const setupMount = (props = {}, state = null) => {
+  const setupProps = { ...defaultProps, ...props };
+  const wrapper = mount(<PluginsRaw {...setupProps} />);
   if (state) wrapper.setState(state);
   return wrapper;
 };
@@ -46,4 +55,36 @@ test('doesnt render plugin type list menu options when logged out', () => {
   const wrapper = setup();
   const pluginTypeListOptions = TestUtil.findByDataTestAttrVal(wrapper, 'plugin-type-list-option');
   expect(pluginTypeListOptions.length).toBe(0);
+});
+
+test('renders plugin link correctly', (done) => {
+  moxios.install(api);
+  const response =
+    [{
+      id_plugin: 1,
+      company: 'Test Plugin Company',
+      name: 'Test Plugin',
+      url: 'http://www.test.com/',
+    }];
+  let wrapper;
+  act(() => {
+    wrapper = setupMount();
+  });
+  moxios.wait(async () => {
+    let request = moxios.requests.mostRecent()
+    await act(async () => {
+      request.respondWith({
+        status: 200,
+        response,
+      }).then(response => {
+        wrapper.update();
+        const pluginLink = TestUtil.findByDataTestAttrVal(wrapper, 'plugin-link');
+        expect(pluginLink.text()).toBe(response.data[0].name);
+        expect(pluginLink.prop('href')).toBe(response.data[0].url);
+        done();
+      }).catch(error => {
+        done.fail(error);
+      })
+    });
+  });
 });
